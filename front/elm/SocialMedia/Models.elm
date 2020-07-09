b4134acba883428a556ca7a528b90819c2587c1a
Pamplemousse <xav.maso@gmail.com>
2020-07-09 07:53:46 -0700

    Remove useless feature since 2c087d3
    Because only the FB link had the need for a "local redirection".

--- front/elm/SocialMedia/Models.elm
+++ front/elm/SocialMedia/Models.elm
@@ -5,7 +5,6 @@ import Messages exposing (Msg(..))
 
 type alias SocialMedium =
     { altText : String
-    , clickAction : Maybe Msg
     , hrefTarget : String
     , imageClass : String
     }
@@ -16,26 +15,21 @@ initialSocialMedia =
     [ { hrefTarget = "mailto:xavier.maso@net-c.com"
       , imageClass = "fa-envelope-o"
       , altText = "Email image link mailing to xavier.maso@net-c.com ."
-      , clickAction = Nothing
       }
     , { hrefTarget = "https://mamot.fr/@Pamplemouss_"
       , imageClass = "fa-mastodon"
       , altText = "Mastodon image link for @Pamplemouss_."
-      , clickAction = Nothing
       }
     , { hrefTarget = "https://twitter.com/Pamplemouss_"
       , imageClass = "fa-twitter"
       , altText = "Twitter image link for @Pamplemouss_."
-      , clickAction = Nothing
       }
     , { hrefTarget = "https://github.com/Pamplemousse"
       , imageClass = "fa-github"
       , altText = "Github image link for Pamplemousse's account."
-      , clickAction = Nothing
       }
     , { hrefTarget = "https://www.linkedin.com/pub/xavier-maso/39/4a/96b"
       , imageClass = "fa-linkedin"
       , altText = "Linkedin image link for Xavier Maso's linkedin profile."
-      , clickAction = Nothing
       }
     ]
--- front/elm/SocialMedia/View.elm
+++ front/elm/SocialMedia/View.elm
@@ -2,7 +2,6 @@ module SocialMedia.View exposing (view)
 
 import Html exposing (Html, a, div, i)
 import Html.Attributes exposing (alt, class, href, target)
-import Html.Events exposing (onClick)
 import Messages exposing (..)
 import Models exposing (Model)
 import SocialMedia.Models exposing (SocialMedium)
@@ -10,16 +9,7 @@ import SocialMedia.Models exposing (SocialMedium)
 
 formatSocialLogo : SocialMedium -> Html Msg
 formatSocialLogo socialMedium =
-    let
-        attribute =
-            case socialMedium.clickAction of
-                Just action ->
-                    onClick action
-
-                _ ->
-                    target "_blank"
-    in
-    a [ href socialMedium.hrefTarget, attribute ]
+    a [ href socialMedium.hrefTarget, target "_blank" ]
         [ i [ class ("fa social-icon " ++ socialMedium.imageClass), alt socialMedium.altText ] []
         ]
 
