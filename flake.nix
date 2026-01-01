421f8b579e5235458407e55589f2d48241061c11
Pamplemousse <git@xaviermaso.com>
2026-01-01 11:43:14 +0100

    Bump nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOs/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-25.11";
   };
 
   outputs = { self, flake-utils, nixpkgs }: flake-utils.lib.eachDefaultSystem (system:
